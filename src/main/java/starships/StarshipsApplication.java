6282a95a4b00abecebd427cdaeac1d267a32083b
Timothy Jeffcoat <timothyjeffcoat@timothys-imac-9.home>
2015-09-14 17:21:15 -0600

    Initial creation of Spring Boot Application

---
+++ src/main/java/starships/StarshipsApplication.java
@@ -0,0 +1,12 @@
+package starships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarshipsApplication.class, args);
+    }
+}
