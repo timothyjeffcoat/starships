f81d464ca4aaeb9bbe7713f3fcc0447319a5f667
Timothy Jeffcoat <tim@jeffcoat.net>
2015-09-14 17:43:36 -0600

    Added RestTemplate to access Star Wars API to the service layer

--- src/main/java/net/jeffcoat/starships/web/StarShipsService.java
+++ src/main/java/net/jeffcoat/starships/web/StarShipsService.java
@@ -1,5 +1,33 @@
 package net.jeffcoat.starships.web;
 
-public class StarShipsService {
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+
+@Service
+public class StarShipsService {
+	private RestTemplate restTemplate;
+
+	
+	public StarShipsService(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);        
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(HttpClients.createDefault());
+        
+        restTemplate = new RestTemplate(requestFactory);
+		
+	}
+	public RestTemplate getRestTemplate() {
+		return restTemplate;
+	}
+
+	public void setRestTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
 }
